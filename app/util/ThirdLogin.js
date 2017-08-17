e554c5cfa166ed6675aaaa8a42c07cee2ed23885
hanson <382349819@qq.com>
2017-08-17 19:08:39 +0800

    2017081701
    QQ登陆新增unionid

--- app/util/ThirdLogin.js
+++ app/util/ThirdLogin.js
@@ -4,6 +4,13 @@ import StorageLoginInfo from '../config/storageLogin'
 import * as QQAPI from 'react-native-qq';
 import * as WechatAPI from 'react-native-wx';
 
+function getCallback(Func) {
+    return Func
+}
+function callback(data) {
+    return data;
+}
+
 module.exports = {
     _qqlogin(that) {
         QQAPI.login()
@@ -11,39 +18,60 @@ module.exports = {
                 let access_token = result.access_token;
                 let oauth_consumer_key = result.oauth_consumer_key;
                 let openid = result.openid;
-                let url = 'https://graph.qq.com/user/get_user_info?access_token=' + access_token + '&oauth_consumer_key=' + oauth_consumer_key + '&openid=' + openid;
+                let url = 'https://graph.qq.com/oauth2.0/me?access_token=' + access_token + '&unionid=1';
                 fetch(url)
                     .then((response) => {
                         if (response.ok) {
-                            response.json()
+                            response.text()
                                 .then((responseData) => {
+                                    let callbackData = getCallback(eval(responseData));
+                                    let url22 = 'https://graph.qq.com/user/get_user_info?access_token=' + access_token + '&oauth_consumer_key=' + oauth_consumer_key + '&openid=' + openid;
+                                    fetch(url22)
+                                        .then((ress) => {
+                                            if (ress.ok) {
+                                                ress.json()
+                                                    .then((ressData) => {
+                                                        if (ressData.ret == 0) {
 
-                                    if (responseData.ret == 0) {
+                                                            let urlN = Api.getUserinfo + '?fromtype=qq&connectid=' + result.openid + '&username=' + ressData.nickname + '&unionid=' + callbackData.unionid;;
 
-                                        let urlN = Api.getUserinfo + '?fromtype=qq&connectid=' + result.openid + '&username=' + responseData.nickname;
+                                                            fetch(urlN)
+                                                                .then((res) => {
+                                                                    if (res.ok) {
+                                                                        res.json()
+                                                                            .then((resData) => {
+                                                                                if (resData.result == 1) {
+                                                                                    StorageLoginInfo.storageSave(resData);
+                                                                                    that.goBackSuccee();
+                                                                                }
+                                                                                else {
+                                                                                    alert('提示', resData.resultmsg)
+                                                                                }
+                                                                            })
+                                                                    }
+                                                                    else {
+                                                                        console.log('网络请求失败')
+                                                                    }
+                                                                })
+                                                                .catch((error) => {
+                                                                    console.log('error:', error)
+                                                                })
+                                                        }
 
-                                        fetch(urlN)
-                                            .then((res) => {
-                                                if (res.ok) {
-                                                    res.json()
-                                                        .then((resData) => {
-                                                            if (resData.result == 1) {
-                                                                StorageLoginInfo.storageSave(resData);
-                                                                that.goBackSuccee();
-                                                            }
-                                                            else {
-                                                                alert('提示', resData.resultmsg)
-                                                            }
-                                                        })
-                                                }
-                                                else {
-                                                    console.log('网络请求失败')
-                                                }
-                                            })
-                                            .catch((error) => {
-                                                console.log('error:', error)
-                                            })
-                                    }
+
+
+                                                    })
+                                            }
+                                            else {
+                                                console.log('网络请求失败')
+                                            }
+                                        })
+                                        .catch((error) => {
+                                            console.log('error:', error)
+                                        })
+
+
+
                                 })
                         }
                         else {
